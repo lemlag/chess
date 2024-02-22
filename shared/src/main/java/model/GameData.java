77edf76e8cfb384531549c11a183de9cb4ed8a4a
John Rowberry <jsr74@byu.edu>
2024-02-21 17:35:07 -0700

    Copied in the starter code for Lab 03

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,11 @@
+package model;
+
+import chess.ChessGame;
+
+public class GameData {
+    String whiteUsername;
+    String blackUsername;
+    String gameName;
+    int gameID;
+    ChessGame game;
+}
