15d0b85266bf1fd3ba1df166fa9164abe4c46a6a
John Rowberry <jsr74@byu.edu>
2024-03-17 00:09:33 -0600

    Draw header complete

--- client/src/main/java/ui/DrawChessBoard.java
+++ client/src/main/java/ui/DrawChessBoard.java
@@ -10,6 +10,8 @@ import static ui.EscapeSequences.*;
 public class DrawChessBoard {
 
     private static final int BOARD_SIZE_IN_SQUARES = 10;
+
+    private static final int BOARD_INDEX = BOARD_SIZE_IN_SQUARES - 1;
     private static final int SQUARE_SIZE_IN_CHARS = 3;
     private static final int LINE_WIDTH_IN_CHARS = 1;
 
@@ -23,9 +25,33 @@ public class DrawChessBoard {
 
     private void drawHeader(ChessGame.TeamColor player, PrintStream out) {
         setHeaderColors(out);
+
+        String[] headers = { "   ", " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h ", "   " };
+        for (int col = 0; col < BOARD_SIZE_IN_SQUARES; col++){
+            if(player == ChessGame.TeamColor.WHITE) {
+                out.print(headers[col]);
+            } else{
+                out.print(headers[BOARD_INDEX - col]);
+            }
+        }
+
+        out.println();
+    }
+
+    private void drawLabel(PrintStream out, String header) {
     }
 
     private void setHeaderColors(PrintStream out) {
-        out.print()
+        out.print(SET_BG_COLOR_DARK_GREY);
+        out.print(SET_TEXT_COLOR_GREEN);
     }
+
+    private void setBlackSquare(PrintStream out){
+        out.print(SET_BG_COLOR_BLACK);
+    }
+
+    private void setWhiteSquare(PrintStream out){
+        out.print(SET_BG_COLOR_WHITE);
+    }
+
 }
